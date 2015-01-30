0e64b864ebfd9000f1f931b017e1b1403568c61f
Brady Somerville <brady.somerville@gmail.com>
2015-01-30 16:24:01 -0600

    amt_list should be the extended price - so multiply by qty

--- app/models/spree/order/rakuten_linkshare.rb
+++ app/models/spree/order/rakuten_linkshare.rb
@@ -10,7 +10,7 @@ module Spree
 
     def rakuten_amt_list
       # TODO: make sure that line-item level and order-level promos are considered
-      line_items.map { |li| ((li.price.to_f + li.promo_total.to_f) * 100).to_i }.join('|')
+      line_items.map { |li| ((li.price.to_f + li.promo_total.to_f) * li.quantity * 100).to_i }.join('|')
     end
 
     def rakuten_name_list
