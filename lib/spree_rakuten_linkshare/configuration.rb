1e7a415a91b1c98ff763a03495c292b774fc4cc0
Brady Somerville <brady.somerville@gmail.com>
2015-01-30 10:41:54 -0600

    remove debug output

--- lib/spree_rakuten_linkshare/configuration.rb
+++ lib/spree_rakuten_linkshare/configuration.rb
@@ -1,6 +1,5 @@
 module SpreeRakutenLinkshare
   class Configuration < Spree::Preferences::Configuration
-    puts "I am in SpreeRakutenLinkshare::Configuration"
     preference :merchant_id, :string
   end
 end
