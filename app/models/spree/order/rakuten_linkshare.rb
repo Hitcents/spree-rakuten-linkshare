e26a19c7458e7237f02a16a06c227a0cda255dd7
Brady Somerville <brady.somerville@gmail.com>
2015-01-30 11:21:23 -0600

    Add tracking pixel to order confirmation page

---
+++ app/models/spree/order/rakuten_linkshare.rb
@@ -0,0 +1,20 @@
+module Spree
+  module Order::RakutenLinkshare
+    def rakuten_sku_list
+      line_items.map { |li| li.variant.sku.gsub('|', ' ') }.join('|')
+    end
+
+    def rakuten_qty_list
+      line_items.map { |li| li.quantity }.join('|')
+    end
+
+    def rakuten_amt_list
+      # TODO: make sure that line-item level and order-level promos are considered
+      line_items.map { |li| (li.price.to_f * 100).to_i }.join('|')
+    end
+
+    def rakuten_name_list
+      line_items.map { |li| li.variant.name.gsub('|', '') }.join('|')
+    end
+  end
+end
